7d4ae7426d0db69be10554f903a39bbd697dfabe
Rafael Langoni <langoninaves@gmail.com>
2022-05-30 20:31:18 -0300

    refatorando while por for

--- main.js
+++ main.js
@@ -3,18 +3,15 @@ function tocaSom(idElementoAudio){
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
-let contador = 0;
-
 /*o que tem de semelhante nos códigos é a parte final do nome da classe;
 dessa forma, podemos pegar sempre o indice 1, que é a parte variável e passá-lo via template string
 para o idAudio que será executado, sendo trocado dinamicamente conforme necessário
 */
-while(contador < listaDeTeclas.length){
+for(let contador = 0; contador < listaDeTeclas.length; contador++){
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`;
     tecla.onclick = () =>{
         tocaSom(idAudio);
     }
-    contador++;
 }
