c163cecc11cfb4d5b835a6377a130d8cf5a1ba13
Rafael Langoni <langoninaves@gmail.com>
2022-05-30 20:25:40 -0300

    pegando dinamicamente o valor para execucao do audio

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
+let contador = 0;
+
+/*o que tem de semelhante nos códigos é a parte final do nome da classe;
+dessa forma, podemos pegar sempre o indice 1, que é a parte variável e passá-lo via template string
+para o idAudio que será executado, sendo trocado dinamicamente conforme necessário
+*/
+while(contador < listaDeTeclas.length){
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+    tecla.onclick = () =>{
+        tocaSom(idAudio);
+    }
+    contador++;
+}
