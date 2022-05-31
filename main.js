fe366a1ad5812b894d7b28124a1995640d92901d
Rafael Langoni <langoninaves@gmail.com>
2022-05-30 21:16:42 -0300

    finalizando o projeto

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 function tocaSom(idElementoAudio){
-    document.querySelector(idElementoAudio).play();
+    const elemento = document.querySelector(idElementoAudio);
+    if(elemento && elemento.localName === 'audio'){
+        elemento.play();
+    }else{
+        console.log('Elemento ou seletor não encontrado')
+    }
 }
-
 const listaDeTeclas = document.querySelectorAll('.tecla');
 /*o que tem de semelhante nos códigos é a parte final do nome da classe;
 dessa forma, podemos pegar sempre o indice 1, que é a parte variável e passá-lo via template string
@@ -13,5 +17,15 @@ for(let contador = 0; contador < listaDeTeclas.length; contador++){
     const idAudio = `#som_${instrumento}`;
     tecla.onclick = () =>{
         tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = (evento) =>{
+        if(evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = () =>{
+        tecla.classList.remove('ativa');
     }
 }
